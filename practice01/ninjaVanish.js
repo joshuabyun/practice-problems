ac5c141d096fe609143ce719bde4dcb97084a210
Joshua Byun <byunsung89@gmail.com>
2016-08-14 13:17:38 -0700

    ninja vanish prototype - with actual pictures, front and back - and expanding cloud divs

--- practice01/ninjaVanish.js
+++ practice01/ninjaVanish.js
@@ -1,17 +1,26 @@
 /**
  * Created by Gina on 8/13/2016.
  */
-$(document).ready(function(){
-   spinDiv();
+$(document).ready(function () {
+    spinDiv();
 });
-function spinDiv(){
-    $('.picture').click(function(e){
-        $(".cloud").addClass('divSpin');
-        var clickedElement = $(".cloud");
-        console.log("clicked element",clickedElement);
-        setTimeout(function(){
-            clickedElement.removeClass('divSpin');
-            console.log("clickedElement after timeout", clickedElement);
-        },2500);
+function spinDiv() {
+    $('.pictureBack').click(function () {
+        $('.pictureBack').fadeOut();
+        setTimeout(function () {
+            $('.picture').fadeOut();
+            $(".cloud").addClass('divExpandLg');
+            $(".cloudSpin").addClass('divSpin');
+            $(".cloudSm").addClass('divExpandSm');
+            setTimeout(function () {
+                $('.pictureBack').fadeIn();
+                setTimeout(function(){
+                    $('.picture').fadeIn();//need a fade in that comes in bit later
+                    $(".cloud").removeClass('divExpandLg');
+                    $(".cloudSm").removeClass('divExpandSm');
+                    $(".cloudSpin").removeClass('divSpin');
+                },200)
+            }, 1500)
+        }, 1500);
     })
 }
